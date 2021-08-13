4d134b74587651bbc54f8d6ca5708ecd956f76b6
atreyu <78748085+atreyu-1991@users.noreply.github.com>
2021-08-13 02:43:53 -0300

    Add files via upload

---
+++ entre-numero.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(){
+	int numero;
+	printf("Entre com um numero inteiro:\n");
+	scanf("%d", &numero);
+	printf("O numero informado foi: %d", numero);
+}
---
+++ teste.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+	char ch1, ch2, ch3;
+	ch1 = 'T';
+	ch2 = 'o';
+	ch3 = 'C';
+	printf("%creinand%c linguagem %c.\n", ch1, ch2, ch3);
+}
